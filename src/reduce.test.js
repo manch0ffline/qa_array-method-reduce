ea59a65e0b40608e13520c9967874f760004a8c4
Roman Romanchuk <roma.roman4uk.48@gmail.com>
2025-05-24 19:23:13 +0200

    solution

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -11,9 +11,36 @@ describe('reduce', () => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {
+  it('should correctly sum numbers with initial value', () => {
+    const result = [1, 2, 3].reduce2((acc, val) => acc + val, 0);
 
+    expect(result).toBe(6);
   });
 
-  // Add tests here
+  it('should return the initial value for an empty array', () => {
+    const result = [].reduce2((acc, val) => acc + val, 0);
+
+    expect(result).toBe(0);
+  });
+
+  it('should correctly work without an initial value', () => {
+    const result = [1, 2, 3].reduce2((acc, val) => acc + val);
+
+    expect(result).toBe(6);
+  });
+
+  it('should call the callback the correct number of times', () => {
+    const callback = jest.fn((acc, val) => acc + val);
+    const arr = [1, 2, 3, 4, 5];
+
+    arr.reduce2(callback, 0);
+
+    expect(callback).toHaveBeenCalledTimes(5);
+  });
+
+  it('should correctly concatenate strings', () => {
+    const result = ['Hello', 'world', '!'].reduce2((acc, val) => acc + val, '');
+
+    expect(result).toBe('Helloworld!');
+  });
 });
